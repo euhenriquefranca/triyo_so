f1c9c405bfcbec95c43ec00e157f2a3d7aa8cfe0
Rafael Abreu <web.nogs@gmail.com>
2020-11-13 01:49:14 -0300

    config orders and create model client

--- database/migrations/1605239835724_order_of_services_schema.js
+++ database/migrations/1605239835724_order_of_services_schema.js
@@ -7,6 +7,21 @@ class OrderOfServicesSchema extends Schema {
   up () {
     this.create('order_of_services', (table) => {
       table.increments()
+      table
+        .integer('user_id')
+        .unsigned()
+        .notNullable()
+        .references('id')
+        .inTable('users')
+        .onUpdate('CASCADE')
+        .onDelete('CASCADE')
+
+      table.integer('os_number').notNullable().unique()
+      table.string('reason_called', 100).notNullable()
+      table.text('message', 250)
+      table.string('place_of_performance').notNullable()
+      table.datetime('start_date_os', { precision: 6 }).notNullable()
+      table.datetime('end_date_os', { precision: 6 }).notNullable()
       table.timestamps()
     })
   }
